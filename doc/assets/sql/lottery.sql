c0060c30bec6aeaddca8b7635ff6b92d13255e59
Allan W <10alan0712@gmail.com>
2023-11-27 16:18:41 -0800

    update doc: sql

--- doc/assets/sql/lottery.sql
+++ doc/assets/sql/lottery.sql
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS `activity`;
 CREATE TABLE `activity` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'auto_increment',
   `activity_id` bigint(20) NOT NULL COMMENT 'activity_id',
-  `activity_name` varchar(64) CHARACTER SET latin1 DEFAULT NULL COMMENT 'activity_name',
-  `activity_desc` varchar(128) CHARACTER SET latin1 DEFAULT NULL COMMENT 'activity_desc',
+  `activity_name` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'activity_name',
+  `activity_desc` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'activity_desc',
   `begin_date_time` datetime DEFAULT NULL COMMENT 'begin_date_time',
   `end_date_time` datetime DEFAULT NULL COMMENT 'end_date_time',
   `stock_count` int(11) DEFAULT NULL COMMENT 'stock_count',
   `take_count` int(11) DEFAULT NULL COMMENT 'take_count per person',
-  `state` tinyint(2) DEFAULT NULL COMMENT 'activity state：edit, arraign, withdraw, pass, run, reject, close, open',
-  `creator` varchar(64) CHARACTER SET latin1 DEFAULT NULL COMMENT 'creator',
+  `state` tinyint(2) DEFAULT NULL COMMENT 'activity state：1 Edit, 2 Submit for review, 3 Withdraw for review, 4 Pass, 5 Run (status of worker scan after review passes), 6 Reject, 7 Close, 8 Open',
+  `creator` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'creator',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'create_time',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'update_time',
   PRIMARY KEY (`id`),
@@ -42,7 +42,7 @@ CREATE TABLE `activity` (
 -- Records of activity
 -- ----------------------------
 BEGIN;
-INSERT INTO `activity` VALUES (1, 100001, '????', '?????????', '2023-08-08 08:14:50', '2023-09-08 20:14:50', 100, 10, 0, 'allan', '2023-08-08 08:14:50', '2023-08-08 08:14:50');
+INSERT INTO `activity` VALUES (1, 100001, 'activity name', 'test activity', '2023-08-08 08:14:50', '2023-09-08 20:14:50', 100, 10, 0, 'allan', '2023-08-08 08:14:50', '2023-08-08 08:14:50');
 COMMIT;
 
 -- ----------------------------
@@ -51,10 +51,10 @@ COMMIT;
 DROP TABLE IF EXISTS `award`;
 CREATE TABLE `award` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'auto_increment',
-  `award_id` varchar(64) CHARACTER SET latin1 DEFAULT NULL COMMENT 'award_id',
+  `award_id` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'award_id',
   `award_type` tinyint(4) DEFAULT NULL COMMENT 'award_type(1. description, 2:redeem code, 3:coupon, 4:physical award)',
-  `award_name` varchar(64) CHARACTER SET latin1 DEFAULT NULL COMMENT 'award_name',
-  `award_content` varchar(128) CHARACTER SET latin1 DEFAULT NULL COMMENT 'award_content (text, key, code)',
+  `award_name` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'award_name',
+  `award_content` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'award_content (text, key, code)',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'create_time',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'update_time',
   PRIMARY KEY (`id`)
@@ -78,11 +78,11 @@ DROP TABLE IF EXISTS `strategy`;
 CREATE TABLE `strategy` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'auto_increment',
   `strategy_id` bigint(11) NOT NULL COMMENT 'strategy_id',
-  `strategy_desc` varchar(128) CHARACTER SET latin1 DEFAULT NULL COMMENT 'strategy_desc',
+  `strategy_desc` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'strategy_desc',
   `strategy_mode` tinyint(2) DEFAULT NULL COMMENT 'lottery mode(1:single, 2:entirety)',
   `grant_type` tinyint(2) DEFAULT NULL COMMENT 'grant_type(1:immediately, 2:delay, 3...)',
   `grant_date` datetime DEFAULT NULL COMMENT 'grant_date',
-  `ext_info` varchar(128) CHARACTER SET latin1 DEFAULT NULL COMMENT 'ext_info',
+  `ext_info` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'ext_info',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'create_time',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update_time',
   PRIMARY KEY (`id`),
@@ -93,7 +93,7 @@ CREATE TABLE `strategy` (
 -- Records of strategy
 -- ----------------------------
 BEGIN;
-INSERT INTO `strategy` VALUES (1, 10001, '??????', 1, 1, NULL, '', '2023-09-04 15:34:52', '2023-09-04 15:34:52');
+INSERT INTO `strategy` VALUES (1, 10001, 'test', 1, 1, NULL, '', '2023-09-04 15:34:52', '2023-09-04 15:34:52');
 COMMIT;
 
 -- ----------------------------
@@ -103,8 +103,8 @@ DROP TABLE IF EXISTS `strategy_detail`;
 CREATE TABLE `strategy_detail` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'auto_increment',
   `strategy_id` bigint(11) NOT NULL COMMENT 'strategy_id',
-  `award_id` varchar(64) CHARACTER SET latin1 DEFAULT NULL COMMENT 'award_id',
-  `award_name` varchar(128) CHARACTER SET latin1 DEFAULT NULL COMMENT 'award_name',
+  `award_id` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'award_id',
+  `award_name` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'award_name',
   `award_count` int(11) DEFAULT NULL COMMENT 'award_count',
   `award_surplus_count` int(11) DEFAULT '0' COMMENT 'award_surplus_count',
   `award_rate` decimal(5,2) DEFAULT NULL COMMENT 'probability',
