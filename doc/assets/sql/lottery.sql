46f95adbcb30e54696b7f20d984b06c3c57126f9
Allan W <10alan0712@gmail.com>
2024-01-03 17:38:29 -0800

    sql updates for branch: rule

--- doc/assets/sql/lottery.sql
+++ doc/assets/sql/lottery.sql
@@ -26,13 +26,14 @@ CREATE TABLE `activity` (
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'update_time',
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_activity_id` (`activity_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='activity config';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='activity config';
 
 -- ----------------------------
 -- Records of activity
 -- ----------------------------
 BEGIN;
-INSERT INTO `activity` VALUES (1, 100001, 'activity name', 'test activity', '2020-11-01 00:00:00', '2023-12-31 23:59:59', 100, 88, 10, 10001, 5, 'allan', '2023-08-08 08:14:50', '2023-08-08 08:14:50');
+INSERT INTO `activity` VALUES (1, 100001, 'activity name', 'test activity', '2020-11-01 00:00:00', '2024-12-31 23:59:59', 100, 88, 10, 10001, 5, 'allan', '2023-08-08 08:14:50', '2023-08-08 08:14:50');
+INSERT INTO `activity` VALUES (3, 100002, 'activity name 2', 'test activity', '2021-10-01 00:00:00', '2024-10-30 23:59:59', 100, 100, 10, 10001, 5, 'allan', '2023-10-05 15:49:21', '2023-10-05 15:49:21');
 COMMIT;
 
 -- ----------------------------
@@ -60,6 +61,80 @@ INSERT INTO `award` VALUES (2, '2', 1, 'iPhone', 'Code', '2023-08-15 15:38:05', 
 INSERT INTO `award` VALUES (3, '3', 1, 'iPad', 'Code', '2023-08-15 15:38:05', '2023-08-15 15:38:05');
 INSERT INTO `award` VALUES (4, '4', 1, 'AirPods', 'Code', '2023-08-15 15:38:05', '2023-08-15 15:38:05');
 INSERT INTO `award` VALUES (5, '5', 1, 'Book', 'Code', '2023-08-15 15:38:05', '2023-08-15 15:38:05');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for rule_tree
+-- ----------------------------
+DROP TABLE IF EXISTS `rule_tree`;
+CREATE TABLE `rule_tree` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'primary id',
+  `tree_name` varchar(64) DEFAULT NULL COMMENT 'rule tree id',
+  `tree_desc` varchar(128) DEFAULT NULL COMMENT 'rule tree desc',
+  `tree_root_node_id` bigint(20) DEFAULT NULL COMMENT 'rule tree root node id',
+  `create_time` datetime DEFAULT NULL COMMENT 'create time',
+  `update_time` datetime DEFAULT NULL COMMENT 'update time',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10002 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of rule_tree
+-- ----------------------------
+BEGIN;
+INSERT INTO `rule_tree` VALUES (2110081902, 'rule tree', 'to determine the activity different users can participate', 1, '2023-10-08 15:38:05', '2023-10-08 15:38:05');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for rule_tree_node
+-- ----------------------------
+DROP TABLE IF EXISTS `rule_tree_node`;
+CREATE TABLE `rule_tree_node` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'primary id',
+  `tree_id` int(2) DEFAULT NULL COMMENT 'rule tree id',
+  `node_type` int(2) DEFAULT NULL COMMENT 'node type；1:stem node;2:leaf node',
+  `node_value` varchar(32) DEFAULT NULL COMMENT 'node value',
+  `rule_key` varchar(16) DEFAULT NULL COMMENT 'rule key',
+  `rule_desc` varchar(32) DEFAULT NULL COMMENT 'rule desc',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of rule_tree_node
+-- ----------------------------
+BEGIN;
+INSERT INTO `rule_tree_node` VALUES (1, 2110081902, 1, NULL, 'userGender', 'genders of users');
+INSERT INTO `rule_tree_node` VALUES (11, 2110081902, 1, NULL, 'userAge', 'user age');
+INSERT INTO `rule_tree_node` VALUES (12, 2110081902, 1, NULL, 'userAge', 'user age');
+INSERT INTO `rule_tree_node` VALUES (111, 2110081902, 2, '100001', NULL, NULL);
+INSERT INTO `rule_tree_node` VALUES (112, 2110081902, 2, '100002', NULL, NULL);
+INSERT INTO `rule_tree_node` VALUES (121, 2110081902, 2, '100003', NULL, NULL);
+INSERT INTO `rule_tree_node` VALUES (122, 2110081902, 2, '100004', NULL, NULL);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for rule_tree_node_line
+-- ----------------------------
+DROP TABLE IF EXISTS `rule_tree_node_line`;
+CREATE TABLE `rule_tree_node_line` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'primary key id',
+  `tree_id` bigint(20) DEFAULT NULL COMMENT 'rule tree id',
+  `node_id_from` bigint(20) DEFAULT NULL COMMENT 'node from',
+  `node_id_to` bigint(20) DEFAULT NULL COMMENT 'node to',
+  `rule_limit_type` int(2) DEFAULT NULL COMMENT 'limit type, 1:=;2:>;3:<;4:>=;5<=;6:enum[enum range];7:leaf node',
+  `rule_limit_value` varchar(32) DEFAULT NULL COMMENT 'limit value',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of rule_tree_node_line
+-- ----------------------------
+BEGIN;
+INSERT INTO `rule_tree_node_line` VALUES (1, 2110081902, 1, 11, 1, 'man');
+INSERT INTO `rule_tree_node_line` VALUES (2, 2110081902, 1, 12, 1, 'woman');
+INSERT INTO `rule_tree_node_line` VALUES (3, 2110081902, 11, 111, 3, '25');
+INSERT INTO `rule_tree_node_line` VALUES (4, 2110081902, 11, 112, 4, '25');
+INSERT INTO `rule_tree_node_line` VALUES (5, 2110081902, 12, 121, 3, '25');
+INSERT INTO `rule_tree_node_line` VALUES (6, 2110081902, 12, 122, 4, '25');
 COMMIT;
 
 -- ----------------------------
